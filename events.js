1b785d7614472f353b57db8280de06dd428c64e9
David Vargas Hernandez <david-0610@live.com.mx>
2021-01-20 23:45:10 -0600

    rotation ando escalation done

--- events.js
+++ events.js
@@ -1,56 +1,26 @@
-let x = prompt("Coordenada x: (Frame es de 600x600)");
-let y = prompt("Coordenada y: (Frame es de 600x600)");
+const x = 300;
+const y = 300;
+const rotation = 10;
 
 // LLamando al ID de la figura del HTML
-let figure = document.getElementById("figure");
+const figure = document.getElementById("figure");
 
 figure.style.left = `${x}px`;
 figure.style.top = `${y}px`;
 
 // Evento de translación
 const eventTranslation = () => {
-  const lessTranslation = () => {
-    figure.style.animationName = "lessTranslation";
-    figure.style.animationDuration = "2.5s";
-    figure.style.animationIterationCount = "infinite";
-  };
-
-  const highTranslation = () => {
-    figure.style.animationName = "highTranslation";
-    figure.style.animationDuration = "2.5s";
-    figure.style.animationIterationCount = "infinite";
-  };
-
-  if (x <= 200 || y <= 200) {
-    lessTranslation();
-  } else if (x >= 201 || y >= 201) {
-    highTranslation();
-  }
+  figure.style.animationName = "highTranslation";
+  figure.style.animationDuration = "2.5s";
+  figure.style.animationIterationCount = "infinite";
 };
 
 // Evento de rotación
 const eventRotation = () => {
-  const rotation = prompt("Ingrese los grados: ");
-
-  const lessRotation = (grades) => {
-    figure.style.animationName = "rotation1";
-    figure.style.animationDuration = "3s";
-    figure.style.animationIterationCount = "infinite";
-    figure.style.transform = `rotate(${grades}deg)`;
-  };
-
-  const highRotation = (grades) => {
-    figure.style.animationName = "rotation2";
-    figure.style.animationDuration = "3s";
-    figure.style.animationIterationCount = "infinite";
-    figure.style.transform = `rotate(${grades}deg)`;
-  };
-
-  if (x <= 200 || y <= 200) {
-    lessRotation(rotation);
-  } else if (x >= 201 || y >= 201) {
-    highRotation(rotation);
-  }
+  figure.style.animationName = "rotation2";
+  figure.style.animationDuration = "3s";
+  figure.style.animationIterationCount = "infinite";
+  figure.style.transform = `rotate(${rotation}deg)`;
 };
 
 // Evento de escalación
